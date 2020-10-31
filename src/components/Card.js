0fe9971e378455d837bd03bd1ccb8ad10ab552a1
khatrironit <ronitkhatri1999@gmail.com>
2020-10-31 17:16:54 +0530

    final commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,18 +8,20 @@ export default function CollegeCard({ data }) {
             <Card style={{ width: '90%' }}>
                 <Card.Img variant="top" src={image} />
                 <Card.Body>
-                    <Col xl = {5}>
+                    <Row>
+                    <Col >
                         <Row><Col><p className = "clgname">{data.college_name}</p></Col></Row>
-                        <Row><Col><p>{data.nearest_place[0]}</p></Col></Row>
+                        <Row><Col><p className = "address">{data.nearest_place[0]}</p></Col></Row>
                         <Row><Col><p>{data.famous_nearest_places}</p></Col></Row>
-                        <Row><Col><p>{data.offertext}</p></Col></Row>
+                        <Row><Col><p className = "offer-box">{data.offertext}</p></Col></Row>
                     </Col>
-                    <Col  xl = {5}>
-                        <Row><Col><p>{data.discounted_fees}</p></Col></Row>
-                        <Row><Col><p>{data.discounted_fees}</p></Col></Row>
+                    <Col >
+                        <Row><Col><p>{data.original_fees}</p></Col></Row>
+                        <Row><Col><p classname = "price">{data.discounted_fees}</p></Col></Row>
                         <Row><Col><p>{data.fees_cycle}</p></Col></Row>
                         <Row><Col><p>{data.amenties[0]}</p></Col></Row>
                     </Col>
+                    </Row>
                 </Card.Body>
             </Card>
         </Col>
